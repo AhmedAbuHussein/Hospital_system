f24f904c71ef9095485e4d7f142ab44cf0ca1df4
shymaa hossny <shymaa.hossny.fciscu@gmail.com>
2017-05-10 17:28:59 +0200

    this is the patient report PDF

--- hospital system/src/report/Report.java
+++ hospital system/src/report/Report.java
@@ -4,38 +4,47 @@ package report;
 
 import handler.HandlerFocus;
 import handler.HandlerMotion;
-
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-
-import java.awt.FlowLayout;
-
-import javax.swing.JTextField;
-import javax.swing.JButton;
+import investigation.investigation;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.FileOutputStream;
+import java.sql.ResultSet;
+import java.util.Date;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JTabbedPane;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
-import java.awt.ScrollPane;
-import java.util.Date;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import net.proteanit.sql.DbUtils;
+import nursing.NursInfo;
+import patient.Patient;
+import patient.PatientStatus;
+import pharmacy.Medical;
+import sqlconnection.SQLCon;
+import doctors.Doctor;
 
 public class Report extends JFrame {
 
@@ -48,22 +57,25 @@ public class Report extends JFrame {
 	private JLabel lblDate;
 	private JLabel label;
 	private JLabel lblHeader;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_3;
-	private JTable table;
+	private JTextField txtsearch;
+	private JTextField txtSsn;
+	private JTextField txtPatientName;
+	
+	private JTable table,temp;
 	private HandlerMotion hdm;
 	private HandlerFocus hdf;
-	
+	private JTextArea txtDocReport, txtInves;
+	private String name,specification;
+	private JButton btnReport;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+				EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Report frame = new Report();
+					Report frame = new Report("","");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -75,7 +87,10 @@ public class Report extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Report() {
+	public Report(String name,String spc) {
+		this.name=name;
+		this.specification=spc;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1730, 735);
 		setLocationRelativeTo(null);
@@ -83,27 +98,32 @@ public class Report extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-
-		JMenu mnPatientRegistration = new JMenu("Patient Registration");
+/*********************************1************************/
+  		JMenu mnPatientRegistration = new JMenu("Patient Registration");
+ 
 		menuBar.add(mnPatientRegistration);
+		
+		
 		
 		JMenuItem mntmPatientRegisrations = new JMenuItem("Patient regisrations");
 		mnPatientRegistration.add(mntmPatientRegisrations);
 		
-		JMenuItem mntmSearchPatientDetails = new JMenuItem("Search Patient details");
-		mnPatientRegistration.add(mntmSearchPatientDetails);
+	
 		
+	/*******************************2**********************************/	
 		JMenu mnDoctorManagement = new JMenu("Doctor management");
 		menuBar.add(mnDoctorManagement);
 		
 		JMenuItem mntmDoctorInformation = new JMenuItem("doctor information");
 		mnDoctorManagement.add(mntmDoctorInformation);
 		
+				
 		JMenuItem mntmPatientStatus = new JMenuItem("patient status");
 		mnDoctorManagement.add(mntmPatientStatus);
 		
-		JMenuItem mntmChangePassword = new JMenuItem("change password");
-		mnDoctorManagement.add(mntmChangePassword);
+		
+
+		/****************************3******************************/
 		
 		JMenu mnPharmacyManagement = new JMenu("Pharmacy management");
 		menuBar.add(mnPharmacyManagement);
@@ -111,8 +131,9 @@ public class Report extends JFrame {
 		JMenuItem mntmMedicamentInformation = new JMenuItem("medicament information");
 		mnPharmacyManagement.add(mntmMedicamentInformation);
 		
-		JMenuItem mntmChangePassword_1 = new JMenuItem("change password");
-		mnPharmacyManagement.add(mntmChangePassword_1);
+	
+		
+		/*************************4***************************/
 		
 		JMenu mnInvestigations = new JMenu("Investigations");
 		menuBar.add(mnInvestigations);
@@ -120,11 +141,17 @@ public class Report extends JFrame {
 		JMenuItem mntmInvestigationType = new JMenuItem("investigation type");
 		mnInvestigations.add(mntmInvestigationType);
 		
+		
+
+		
+		
 		JMenuItem mntmPatientStatus_1 = new JMenuItem("patient status");
 		mnInvestigations.add(mntmPatientStatus_1);
 		
-		JMenuItem mntmChangePassword_2 = new JMenuItem("change password");
-		mnInvestigations.add(mntmChangePassword_2);
+		
+		
+		/********************5**********************/
+		
 		
 		JMenu mnNursing = new JMenu("Nursing");
 		menuBar.add(mnNursing);
@@ -132,11 +159,18 @@ public class Report extends JFrame {
 		JMenuItem mntmNurseInformation = new JMenuItem("nurse information");
 		mnNursing.add(mntmNurseInformation);
 		
+		
+		
+
+		
+		
+		
 		JMenuItem mntmPatientStatus_2 = new JMenuItem("patient status");
 		mnNursing.add(mntmPatientStatus_2);
 		
-		JMenuItem mntmChangePassword_3 = new JMenuItem("change password");
-		mnNursing.add(mntmChangePassword_3);
+		
+
+		/************************6***************/
 		
 		JMenu mnAdminManagement = new JMenu("Admin management");
 		menuBar.add(mnAdminManagement);
@@ -144,20 +178,34 @@ public class Report extends JFrame {
 		JMenuItem mntmDoctorInformation_1 = new JMenuItem("doctor information");
 		mnAdminManagement.add(mntmDoctorInformation_1);
 		
+		
+
+
 		JMenuItem mntmNurseInformation_1 = new JMenuItem("nurse information");
 		mnAdminManagement.add(mntmNurseInformation_1);
 		
+		
+		
+
 		JMenuItem mntmPharmacyInformation = new JMenuItem("pharmacy information");
 		mnAdminManagement.add(mntmPharmacyInformation);
 		
+		
+
+
 		JMenuItem mntmInvestigationInformation = new JMenuItem("investigation information");
 		mnAdminManagement.add(mntmInvestigationInformation);
 		
+		
+
 		JMenuItem mntmPatientInformation = new JMenuItem("patient information");
 		mnAdminManagement.add(mntmPatientInformation);
 		
-		JMenuItem mntmChangePassword_4 = new JMenuItem("change password");
-		mnAdminManagement.add(mntmChangePassword_4);
+		
+		
+		
+
+		/********************************7****************************/
 		
 		JMenu mnReport = new JMenu(" Reports");
 		menuBar.add(mnReport);
@@ -165,15 +213,8 @@ public class Report extends JFrame {
 		JMenuItem mntmPatientReport = new JMenuItem("patient report");
 		mnReport.add(mntmPatientReport);
 		
-		JMenuItem mntmPharmacyReport = new JMenuItem("pharmacy report");
-		mnReport.add(mntmPharmacyReport);
 		
-		JMenuItem mntmLaundryReport = new JMenuItem("laundry report");
-		mnReport.add(mntmLaundryReport);
 		
-		JMenu mnLaundryManagement = new JMenu("Laundry management");
-		menuBar.add(mnLaundryManagement);
-
 		
 		
 		contentPane = new Panal();
@@ -221,25 +262,65 @@ public class Report extends JFrame {
 			
 			//***************************************Footer ************************************************
 			
-		    label = new JLabel("");
-			label.setIcon(new ImageIcon(getClass().getResource("/Images/header.png")));
-			label.setBounds(0, 629, 1387, 78);
-			contentPane.add(label);
+	    label = new JLabel("");
+		label.setIcon(new ImageIcon(getClass().getResource("/Images/header.png")));
+		label.setBounds(0, 629, 1387, 72);
+		contentPane.add(label);
 			
 		
 		
-		
-		
-		
-		
-	
-		textField = new JTextField();
-		textField.setBounds(543, 104, 202, 32);
-		textField.addFocusListener(hdf);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtsearch = new JTextField();
+		txtsearch.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+			
+					int ssn = Integer.parseInt(txtsearch.getText());
+					
+					String sql;
+					
+					if(txtsearch.getText().trim().equals(""))
+						sql = "select * from patient_statues";
+					else
+					sql = "select * from patient_statues where ssn ="+ssn;
+					
+					ResultSet rs = SQLCon.SQLConnection(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+							
+			}
+				
+			}
+		);
+		txtsearch.setBounds(543, 104, 202, 32);
+		txtsearch.addFocusListener(hdf);
+		contentPane.add(txtsearch);
+		txtsearch.setColumns(10);
+		txtsearch.addActionListener(new The_Handeler());
 		
 		JButton btnNewButton = new JButton("Search");
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				try{
+					int ssn = Integer.parseInt(txtsearch.getText());
+					
+					String sql;
+					
+					if(txtsearch.getText().trim().equals(""))
+						sql = "select * from patient_statues";
+					else
+					sql = "select * from patient_statues where ssn ="+ssn;
+					
+					ResultSet rs = SQLCon.SQLConnection(sql);
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+
+				
+				}catch(NumberFormatException e){
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
+							
+			}
+		});
+		btnNewButton.addActionListener(new The_Handeler());
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnNewButton.setForeground( Color.white);
 		btnNewButton.addMouseListener(hdm);
@@ -247,11 +328,30 @@ public class Report extends JFrame {
 		btnNewButton.setBounds(781, 103, 125, 32);
 		contentPane.add(btnNewButton);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(156, 219, 413, 40);
-		textField_1.addFocusListener(hdf);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		txtSsn = new JTextField();
+		txtSsn.setBounds(156, 219, 413, 40);
+		txtSsn.addFocusListener(hdf);
+		contentPane.add(txtSsn);
+		txtSsn.setColumns(10);
+		
+		
+		JLabel lblSsn = new JLabel("search by ssn");
+		lblSsn.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSsn.setForeground(new Color(0, 0, 0));
+		lblSsn.setBounds(408, 106, 125, 28);
+		lblSsn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblSsn.setBackground(new Color(20,30,60));		
+		contentPane.add(lblSsn);
+		
 		
 		JLabel lblNewLabel = new JLabel("Patient SSN");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -263,11 +363,11 @@ public class Report extends JFrame {
 		lblNewLabel_1.setBounds(156, 278, 114, 23);
 		contentPane.add(lblNewLabel_1);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(781, 219, 413, 40);
-		textField_3.addFocusListener(hdf);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		txtPatientName = new JTextField();
+		txtPatientName.setBounds(781, 219, 413, 40);
+		txtPatientName.addFocusListener(hdf);
+		contentPane.add(txtPatientName);
+		txtPatientName.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Patient Name");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -279,13 +379,17 @@ public class Report extends JFrame {
 		lblNewLabel_3.setBounds(781, 283, 172, 18);
 		contentPane.add(lblNewLabel_3);
 		
-		JButton btnNewButton_1 = new JButton("Report PDF");
-		btnNewButton_1.setForeground( Color.white);
-		btnNewButton_1.addMouseListener(hdm);
-		btnNewButton_1.setBackground(new Color(20,30,60));
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnNewButton_1.setBounds(620, 352, 125, 32);
-		contentPane.add(btnNewButton_1);
+		btnReport = new JButton("Report PDF");
+		btnReport.addActionListener(new The_Handeler());
+		btnReport.setForeground( Color.white);
+		btnReport.addMouseListener(hdm);
+		btnReport.setBackground(new Color(20,30,60));
+		btnReport.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnReport.setBounds(620, 352, 125, 32);
+		contentPane.add(btnReport);
+		
+		
+		
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(505, 648, -278, -134);
@@ -298,6 +402,7 @@ public class Report extends JFrame {
 		JScrollPane scrollPane_2 = new JScrollPane();
 		scrollPane_2.setBounds(156, 414, 1038, 216);
 		contentPane.add(scrollPane_2);
+		temp = new JTable();
 		
 		table = new JTable(){
 		
@@ -336,32 +441,141 @@ public class Report extends JFrame {
 				"New column", "New column", "New column", "New column", "New column"
 			}
 		));
+		ResultSet rs = SQLCon.SQLConnection("select * from patient_statues"); 
+		table.setModel(DbUtils.resultSetToTableModel(rs));
+		
+		
+		
+		
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e){
+			
+			
+				
+				int row = table.getSelectedRow();
+				txtSsn.setText(table.getValueAt(row, 0).toString());
+				txtDocReport.setText(table.getValueAt(row, 1).toString());
+				txtInves.setText(table.getValueAt(row, 2).toString());
+				
+				ResultSet rss = SQLCon.SQLConnection("select name from patient "
+						+ "where SSN = "+Integer.parseInt(table.getValueAt(row, 0).toString()));
+				temp.setModel(DbUtils.resultSetToTableModel(rss));
+				txtPatientName.setText(temp.getValueAt(0, 0).toString());
+			}
+		});
+		
 		scrollPane_2.setViewportView(table);
 		
 		JScrollPane scrollPane_3 = new JScrollPane();
 		scrollPane_3.setBounds(156, 312, 413, 72);
 		contentPane.add(scrollPane_3);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		textArea.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
-		textArea.addFocusListener(hdf);
-		textArea.setWrapStyleWord(true);
-		scrollPane_3.setViewportView(textArea);
+		txtDocReport = new JTextArea();
+		txtDocReport.setLineWrap(true);
+		txtDocReport.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
+		txtDocReport.addFocusListener(hdf);
+		txtDocReport.setWrapStyleWord(true);
+		scrollPane_3.setViewportView(txtDocReport);
 		
 		JScrollPane scrollPane_4 = new JScrollPane();
 		scrollPane_4.setBounds(781, 312, 413, 72);
 		contentPane.add(scrollPane_4);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setLineWrap(true);
-		textArea_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
-		textArea_1.addFocusListener(hdf);
-		textArea_1.setWrapStyleWord(true);
-		scrollPane_4.setViewportView(textArea_1);
+		txtInves = new JTextArea();
+		txtInves.setLineWrap(true);
+		txtInves.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
+		txtInves.addFocusListener(hdf);
+		txtInves.setWrapStyleWord(true);
+		scrollPane_4.setViewportView(txtInves);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				txtDocReport.setText("");
+				txtPatientName.setText("");
+				txtInves.setText("");
+				txtSsn.setText("");
+			}
+		});
+		btnClear.setForeground(Color.WHITE);
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnClear.setBackground(new Color(20, 30, 60));
+		btnClear.setBounds(620, 309, 125, 32);
+		contentPane.add(btnClear);
+		
+		JLabel lblNewLabel_4 = new JLabel("Dr : ");
+		lblNewLabel_4.setBounds(10, 650, 46, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setBounds(66, 648, 46, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblDr = new JLabel("Dr :");
+		lblDr.setBounds(39, 650, 46, 14);
+		contentPane.add(lblDr);
+		
+		
 		
 		/*JLabel lblNewLabel_5 = new JLabel(new Date()+"");
 		lblNewLabel_5.setBounds(543, 71, 202, 22);
 		contentPane.add(lblNewLabel_5);*/
+	}
+	private class The_Handeler implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource()==btnReport){
+				
+				JTable temp = new JTable();
+				if(!txtSsn.getText().trim().equals("")){
+					temp.setModel(DbUtils.resultSetToTableModel(SQLCon.SQLConnection("select * from patient"
+							+ " where SSN = "+Integer.parseInt(txtSsn.getText()))));
+					String[]listString ={"SSN : "+temp.getValueAt(0, 0).toString(),"Name : "+temp.getValueAt(0, 1).toString()
+										,"Gender : "+temp.getValueAt(0, 2).toString(),"Phone : "+temp.getValueAt(0, 3).toString()
+										,"Age : "+temp.getValueAt(0, 4).toString(),"Address : "+temp.getValueAt(0, 5).toString()
+										,"Marital Statues : "+temp.getValueAt(0, 6).toString(),"Weight : "+temp.getValueAt(0, 7).toString(),};
+					
+					temp.setModel(DbUtils.resultSetToTableModel(SQLCon.SQLConnection("select * from patient_statues"
+							+ " where SSN = "+Integer.parseInt(txtSsn.getText()))));
+					String []tableHeader={"SSN","Doctor Report","Investigation report","Investigation Type"};
+					String []tableData = {table.getValueAt(0, 0).toString(),
+										  table.getValueAt(0, 1).toString(),
+										  table.getValueAt(0, 2).toString(),
+										  table.getValueAt(0, 3).toString()};
+					
+					
+					try {
+						String file ="E:\\"+JOptionPane.showInputDialog(null, "Enter Report File Name")+".pdf";
+						
+			             Document document = new Document();
+			             PdfWriter.getInstance(document, new FileOutputStream(file));
+			             document.open();
+			             TextPDF.addTitlePage(document,name,"");
+			             TextPDF.addContent(document,listString,tableHeader,tableData);
+			             document.close();
+			             JOptionPane.showMessageDialog(null, "Done Successful");
+				     } catch (Exception ee) {
+				             ee.printStackTrace();
+				     }
+				}else{
+					JOptionPane.showMessageDialog(null, "plz select patient in table");
+					return;
+				}
+			}else{
+				ResultSet rss;
+				if(txtsearch.getText().equals(""))
+				{
+					rss=SQLCon.SQLConnection("select * from patient_statues");
+					table.setModel(DbUtils.resultSetToTableModel(rss));
+				}
+				else
+				{
+					rss=SQLCon.SQLConnection("select * from patient_statues where ssn="+Integer.parseInt(txtsearch.getText()));
+					table.setModel(DbUtils.resultSetToTableModel(rss));
+				}
+			}
+		}
 	}
 }
